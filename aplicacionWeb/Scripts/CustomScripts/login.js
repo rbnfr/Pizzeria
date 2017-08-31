9a0c2c45b9aee0a86684dbeaf3d12effd3a5c65b
PC <PC@DESKTOP-613>
2017-08-31 14:22:06 +0200

    Actualización registro

---
+++ aplicacionWeb/Scripts/CustomScripts/login.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('LoginForm', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMessages'])
+    .controller('CtrlLogin', function ($scope) {
+        $scope.vm = {
+            formData: {
+                email: 'correo@ejemplo.com',
+                password: 'foobar'
+            }
+        };
+    });
